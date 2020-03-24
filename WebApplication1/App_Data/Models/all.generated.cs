d6097f5bbe20cd039dfcc8f1754a3d46bc1435c7
KIN LAM <k.lawrence.lam@googlemail.com>
2020-03-24 10:39:22 +0000

    Commit 2

--- WebApplication1/App_Data/Models/all.generated.cs
+++ WebApplication1/App_Data/Models/all.generated.cs
@@ -7,7 +7,7 @@ using  Umbraco.Core.Models.PublishedContent;
 using  Umbraco.Web;
 using  Umbraco.ModelsBuilder.Embedded;
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "3307619bd5248856")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.5")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
